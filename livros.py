9d9566a7f420b0b89b89a71986a1ec7c1b22c409
Mateus-2009 <mateusdrjr10@gmail.com>
2024-07-25 22:14:37 -0300

    comentario

--- livros.py
+++ livros.py
@@ -1,10 +1,30 @@
 import pandas as pd
 
 df = pd.read_csv("tabela_livro.csv")
+#print(df.info())
 
-#print(df)
+"""df_filtrado = df[df["Ano de Publicação"] == 1993]
+print(df_filtrado)
+"""
 
-class Livro:
+livro = {
+    "Título do Livro": "Python para Todos",
+    "Autor": "Charles Severance",
+    "Categoria": "Programação",
+    "Ano de Publicação": 2016,
+    "Ativo": True
+}
+#1 ex
+df = df.append(livro, ignore_index=True)
+
+#2 ex
+livros_programacao = df[df["Categoria"]== "Programação"]
+
+
+
+##
+
+'''class Livro:
     def __init__(self, titulo, autor, categoria, ano, ativo):
         self.titulo = titulo
         self.autor = autor
@@ -18,4 +38,4 @@ livro2 = Livro("Livro 2", "Julio", "Programação",2012, "Sim")
 lista_de_livros = [livro0, livro1, livro2]
 
 for livro in lista_de_livros:
-    print(livro.titulo)
+    print(livro.titulo)'''
