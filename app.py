eb8c1ff4018ff38f458e2e30630397fcfdb4a440
Mateus-2009 <mateusdrjr10@gmail.com>
2024-07-29 22:16:41 -0300

    Descreva as mudanças feitas

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/inicio')
 def ola():
     lista = ['O Senhor dos Anéis', 'Dom Casmurro', 'O Alquimista']
-    return render_template(lista.html, titulo='Listagem de Livros - SENAI', lista_de_livros=lista)
+    return render_template('lista.html', titulo='Listagem de Livros - SENAI', lista_de_livros=lista)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run()
+
