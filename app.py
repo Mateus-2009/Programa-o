1e69e83fde257ce5dc3ec4957b8e052b7509cdf7
Mateus-2009 <mateusdrjr10@gmail.com>
2024-07-30 22:11:13 -0300

    Descreva as mudanças feitas

--- app.py
+++ app.py
@@ -2,10 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/inicio')
+
+@app.route("/inicio")
 def ola():
-    lista = ['O Senhor dos Anéis', 'Dom Casmurro', 'O Alquimista']
-    return render_template('lista.html', titulo='Listagem de Livros - SENAI', lista_de_livros=lista)
+    lista = ["O Senhor dos Anéis", "Dom Casmurro", "O Alquimista"]
+    return render_template(
+        "lista.html", titulo="Listagem de Livros - SENAI", lista_de_livros=lista
+    )
 
-app.run()
 
+@app.route("/curriculo")
+def curriculo():
+    return render_template("curriculo.html")
+
+
+app.run(debug=True)
